0394aeb63ee6c0d19217562e7c95d7a4eae82055
AyurSaanchezL <ayur.sanchez@educa.madrid.org>
2025-11-29 16:53:30 +0100

    v2 animación de entrada y música del main mejoradas + otros

--- scripts/script_main.js
+++ scripts/script_main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById("btnMusic");
     const song = document.getElementById("song");
+    const icon = document.getElementById("icon");
     let muted = true;
 
     button.addEventListener('click', () => {
@@ -9,7 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
             // Si NO está 'muted' (es decir, quieres reproducir)
             song.play();
             console.log("PLAY")
-            icon.textContent = 'pause'; // Cambia el ícono a 'pause'
+            icon.textContent = "pause";
+
         } else {
             // Si está 'muted' (es decir, quieres pausar)
             song.pause();
